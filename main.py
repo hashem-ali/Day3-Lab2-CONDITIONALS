36e1942c64c31cac3af4fbbd68ac8b480ffe1ea8
hashem-ali <hashem.bajabaa1@gmail.com>
2022-08-02 13:28:40 +0300

    Done with Lab

---
+++ main.py
@@ -0,0 +1,9 @@
+my_movie = "Top Gun"
+rating = 3
+score = 72.65
+if rating >= 4 and score > 80:
+    print("Highly recommended")
+elif rating >= 3 and score > 70 :
+    print("I recommended it . It is good")
+elif rating <= 2 and score < 50:
+    print("Don't watch it. It is a waste of time")
